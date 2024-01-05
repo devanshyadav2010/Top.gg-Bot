69c0d0b7c9eaf1a427cadfe919a263c2b5b35153
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2024-01-05 12:08:42 +0530

    Update emotes.js

--- Assets/Config/emotes.js
+++ Assets/Config/emotes.js
@@ -1,8 +1,8 @@
 module.exports = {
   MESSAGE: {
-    y: "<a:MarkChecking:992381788028669992>", // yes 
-    x: "<:Cross:953158094022668338>", // no
-    i: "<:err:985089618657554472>", // info
-    l: "" // loading
+    y: "☑️", // yes 
+    x: "❎", // no
+    i: "ℹ️", // info
+    l: "🌀" // loading
   }
 }

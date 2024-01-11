690b2ab8995116f3b1c0705d0f481a7f07f10781
Devansh Yadav <108845911+devanshyadav2010@users.noreply.github.com>
2024-01-11 22:47:09 +0530

    Update Profile.js
    Added TopGGCard and made streak counting function

--- Commands/Slash/Info/Profile.js
+++ Commands/Slash/Info/Profile.js
@@ -1,4 +1,5 @@
 const TopGG = require(`${process.cwd()}/src/database/TopGG`);
+const { TopGGCard } = require("topggcard");
 
 module.exports = {
   name: "profile",
@@ -6,7 +7,6 @@ module.exports = {
   run: async (client, interaction) => {
     try {
       const userId = interaction.user.id;
-
       const hasVoted = await client.topgg.hasVoted(userId);
 
       let userStats = await TopGG.findOne({ userID: userId });
@@ -19,15 +19,33 @@ module.exports = {
           totalVotes: 0,
           purchasedRoles: [],
         });
-          await userStats.save()
+        await userStats.save();
       }
+
+      const progressPercentage = (userStats.totalVotes / 14) * 100;
+
+      const currentStreak = calculateCurrentStreak(userStats.lastVoteTimestamp, userStats.totalVotes);
+
+      const card = new TopGGCard()
+        .setName(interaction.user.username)
+        .setVotes(userStats.totalVotes)
+        .setColor("auto")
+        .setBrightness(100)
+        .setAvatar(interaction.user.displayAvatarURL({ forceStatic: true }))
+        .setProgress(progressPercentage)
+        .setStreak(currentStreak.toString())
+        .setCurrentStreak(currentStreak.toString())
+        .setRequiredStreak("14")
+        .setShowStreak(true);
+
+      const cardBuffer = await card.build();
 
       const embed = new client.discord.EmbedBuilder()
         .setTitle(`Here Is Your Profile Info!`)
         .setAuthor({ name: interaction.user.username, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) })
         .setThumbnail(interaction.user.displayAvatarURL({ dynamic: true }))
         .addFields(
-          { name: "Voting Streak", value: `${userStats ? userStats.totalVotes || 0 : 0}/14`, inline: true },
+          { name: "Voting Streak", value: `${userStats.totalVotes || 0}/14`, inline: true },
           { name: "Is Voter?", value: `${hasVoted ? "Yes" : "No"}`, inline: true }
         )
         .setColor('0fffc0')
@@ -40,3 +58,18 @@ module.exports = {
     }
   }
 };
+
+function calculateCurrentStreak(lastVoteTimestamp, totalVotes) {
+  const currentDate = new Date();
+  const lastVoteDate = new Date(lastVoteTimestamp);
+
+  const isStreakBroken = Math.abs(currentDate - lastVoteDate) > 24 * 60 * 60 * 1000;
+
+  if (isStreakBroken) {
+    return 0;
+  }
+
+  const streak = Math.floor((currentDate - lastVoteDate) / (24 * 60 * 60 * 1000));
+
+  return streak + totalVotes;
+                 }
